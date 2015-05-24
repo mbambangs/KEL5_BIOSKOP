86c121f480329a1ebc95f3ee2cb7ae8df94a0691
mbambangs <mbambangsulistio@gmail.com>
2015-05-24 12:22:49 +0700

    SEARCH_PATH INCLUDED
    perbaikan di db.php dan def.php agar bisa mengacu ke search_path.

--- common/db.php
+++ common/db.php
@@ -6,6 +6,13 @@
     	$conn_string = "host=".DB_HOST." port=".DB_PORT." dbname=".DB_NAME." user=".DB_USER." password=".DB_PASS;
 //     	echo $conn_string;
 		$dbconn = pg_pconnect($conn_string);
+		if ($dbconn){		
+			$setsearchpath=pg_query($dbconn,"SET search_path to ".DB_SEARCHPATH);
+			echo "koneksi sudah OK dengan search_path ".DB_SEARCHPATH;
+		}else{
+			echo "Error di Koneksi (db.php)! ";
+		}
 		return $dbconn;
     }
+    connectDB();
 ?>
