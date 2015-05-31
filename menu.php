253821d1f2c3399cfbe194e5af6b504a10eda9d6
fitrinababan19 <fitri.nababan19@yahoo.com>
2015-05-31 14:50:46 +0700

    Lihat informasi bioskop
    Masih ada yang kurang sih, tapi query doang, oh ya ehsan jangan lupa
    kelarin yang cari bioskop nya segera ya, biar bisa di test :dancer:
    Semangat ya qaqa.

--- menu.php
+++ menu.php
@@ -3,11 +3,23 @@
 		<div class="top-nav">
 			  <span class="menu"><img src="images/menu.png" alt=""> </span>
 			  <ul>
-				<li class="active"><a href="index.php">HOME</a></li>
-				<li><a href="">THEATRES</a></li>
+			  	<?php if (isset ($_SESSION['home'])){?>
+					<li class="active"><a href="index.php">HOME</a></li>
+				<?php }else{?>
+					<li><a href="index.php">HOME</a></li>
+				<?php }?>
+				<?php if (isset ($_SESSION['theatre'])){?>
+					<li class="active"><a href="theatres.php">THEATRES</a></li>
+				<?php }else{?>
+					<li><a href="theatres.php">THEATRES</a></li>
+				<?php }?>
 				<li><a href="">BOOKING HISTORY</a></li>
-				<?php if(!isset($_SESSION['email'])){?>
-					<li><a href="login.php">LOGIN</a></li>
+				<?php if(!isset($_SESSION['email'])){
+						if(isset($_SESSION['loginmenu'])){?>
+							<li class="active" ><a href="login.php">LOGIN</a></li>
+						<?}else{?>
+						<li><a  href="login.php">LOGIN</a></li>
+					<?php }?>
 				<?php }else{?>
 					<li><a href="controller/logoutaction.php">LOGOUT</a></li>
 				<?php }?>
@@ -33,20 +45,4 @@
           </div>
 		 </div>
 	</div>
-	<!---->
-  	 <div class="header">  	
-		 		<!-- Slider -->				
-				     <div class="slider">
-				      <div class="slider-wrapper theme-default">
-				            <div id="slider" class="nivoSlider">
-				                <img src="images/5.jpg" data-thumb="images/5.jpg" alt="" />
-								<img src="images/6.jpg" data-thumb="images/6.jpg" alt="" />
-				            </div>
-				        </div>
-				          <div class="header_desc">
-				 			 <div class="logo">
-				 				<a href="index.html"><img src="images/logo.png" alt="" /></a>
-							 </div>							
-		     		    </div>
-				   </div>
-			     <!--- End Slider --->
+	
